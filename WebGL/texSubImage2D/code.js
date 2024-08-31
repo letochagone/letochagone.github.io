1e0080ef0282270e8305fa7ffa89c893b7dbc482
Letocha Torgue Philippe <torguephilippe@gmail.com>
2024-08-31 03:19:51 +0200

    Update code.js

--- WebGL/texSubImage2D/code.js
+++ WebGL/texSubImage2D/code.js
@@ -1,31 +1,10 @@
-
 "use strict";
 
 let canvas = document.getElementById("canvas");
+let gl = canvas.getContext("webgl");
 
-let gl = canvas.getContext("webgl",{antialias:false});
-gl.getExtension('OES_texture_float');
-
-
-  let width = 6;
-  let height = 3;
-
-  canvas.style.width="600px";
-  canvas.style.height="300px";
-  canvas.style.imageRendering="pixelated";
-
-
-const PARTICLE_COUNT = 9;
-const PARTICLE_COUNT_SQRT = Math.sqrt(PARTICLE_COUNT);
-const PARTICLE_DATA_SLOTS = 2;
-const PARTICLE_DATA_WIDTH = PARTICLE_COUNT_SQRT * PARTICLE_DATA_SLOTS;
-const PARTICLE_DATA_HEIGHT = PARTICLE_COUNT_SQRT;
-
-gl.canvas.width = width;
-gl.canvas.height = height;
-
-
-  gl.viewport(0, 0, width, height);
+gl.canvas.width = 4;
+gl.canvas.height = 4;
 
 
 let vertex = `
@@ -37,21 +16,25 @@ let vertex = `
       varying vec2 v_texCoord;
 
       void main() {
-
-        gl_Position = vec4(a_position, 0, 1);
+        vec2 zeroToOne = a_position / u_resolution;
+        vec2 zeroToTwo = zeroToOne * 2.0;
+        vec2 clipSpace = zeroToTwo - 1.0;
+        gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+        
+        gl_Position=vec4(a_position,0.0,1.0);
         v_texCoord = a_texCoord;
       }
     `;
 
 let fragment = `
       precision mediump float;
-
       uniform sampler2D u_image;
-
       varying vec2 v_texCoord;
-
       void main() {
         gl_FragColor = texture2D(u_image, v_texCoord);
+        
+        
+      //  gl_FragColor=vec4(0,0,0,1);
       }
     `;
 
@@ -69,7 +52,8 @@ gl.linkProgram(shader);
 
 
 
-
+  let width = 4;
+  let height = 4;
 
   let positionLocation = gl.getAttribLocation(shader, "a_position");
   let texcoordLocation = gl.getAttribLocation(shader, "a_texCoord");
@@ -77,85 +61,96 @@ gl.linkProgram(shader);
   let positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-    -1,-1,    -1,1,  1,-1,    1,-1,   -1,1,  1,1
-
-
+    0, 0,
+    width, 0,
+    0, height,
+    0, height,
+    width, 0,
+    width, height
   ]), gl.STATIC_DRAW);
- 
-
+  
+  
+   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    -1, -1,
+    1, -1,
+    -1, 1,
+    -1,1,
+    1,1,
+    1,-1
+    /*
+    -0.75, 0.75,
+    0.75, 0.75,
+    0.75, -0.75
+    */
+  ]), gl.STATIC_DRAW);
+  
 
   let texcoordBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-
-    /* pourquoi ça ne marche pas ?...
     0.0, 0.0,
     1.0, 0.0,
     0.0, 1.0,
     0.0, 1.0,
+    1.0, 1.0,
     1.0, 0.0,
-    1.0, 1.0,
-    */
-
-    0.0, 1.0,
-    0.0, 0.0,
-    1.0, 1.0,
-    1.0, 1.0,
-    0.0, 0.0,
-    1.0, 0.0
   ]), gl.STATIC_DRAW);
 
+  let texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
 
 
-let   texture= gl.createTexture();
-gl.bindTexture(gl.TEXTURE_2D, texture);
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
 
-const r = [255, 0, 0, 255];
-const g = [0, 255, 0, 255];
-const b=[0,0,255,255];
 
-const y = [255,255,0,255];
-const p = [0,255,255,255];
-
-gl.texImage2D(
-     gl.TEXTURE_2D,
-     0,        // mip level
-     gl.RGBA,  // internal format
-     width,        // width
-     height,        // height
-     0,        // border
-     gl.RGBA,  // data format
-     gl.UNSIGNED_BYTE,   // data type
-     new Uint8Array([    // data
-        ...r, ...g, ...r, ...g, ...r, ...g,
-        ...g, ...r, ...g, ...r, ...g, ...r,
-        ...y, ...g, ...r, ...g, ...r, ...g,
-
-
-     ]));
-
-
-  gl.texSubImage2D(
-    gl.TEXTURE_2D,0,
-    3,
-    1,
-    2,
-    2,
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    4,
+    4,
+    0,
     gl.RGBA,
     gl.UNSIGNED_BYTE,
     new Uint8Array([
-        ...b, ...b, 
-        ...p, ...b, ])
+      255,255,0,255,  255,255,0,255,  255,255,0,255, 127,64,0,255,
+      255,255,0,255,  255,255,0,255,  255,255,0,255, 255,255,0,255,
+      255,255,0,255,  255,255,0,255,  255,255,0,255, 255,255,0,255,
+      255,255,0,255,  255,255,0,255,  255,255,0,255, 255,255,0,255,
+      
+      ]));
+
+
+
+  gl.texSubImage2D(
+    gl.TEXTURE_2D,
+    0,//mip level
+    1,//destX
+    1,//destY
+    3,//width
+    1,//heiht
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    new Uint8Array([
+     255,0,0,255,    0,255,0,255,  0,0,255,255
+     // 255,0,0,255,    0,255,0,255, 
+
+      //la taille de ce tableau NE peut PAS etre inférieure au parametre width
+      ])
   );
 
 
+  let resolutionLocation = gl.getUniformLocation(shader, "u_resolution");
 
+
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  gl.clear(gl.COLOR_BUFFER_BIT);
 
   gl.useProgram(shader);
 
@@ -177,14 +172,31 @@ gl.texImage2D(
   offset = 0;
   gl.vertexAttribPointer(texcoordLocation, size, type, normalize, stride, offset);
 
+  gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
 
-  gl.activeTexture(gl.TEXTURE0);
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  let utexture = gl.getUniformLocation(shader, 'u_image');
 
-  gl.uniform1i(utexture, 0);
 
   gl.drawArrays(gl.TRIANGLES, 0, 6);
+  
+    gl.texSubImage2D(
+    gl.TEXTURE_2D,
+    0,//mip level
+    0,//destX
+    0,//destY
+    1,//width
+    2,//heiht
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    new Uint8Array([
+     127,127,127,255,  127,0,127,255, 
+     // 255,0,0,255,    0,255,0,255, 
+
+      //la taille de ce tableau NE peut PAS etre inférieure au parametre width
+      ])
+  );
+
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+
 
 
   let pixels = new Uint8Array(width * height * 4);
