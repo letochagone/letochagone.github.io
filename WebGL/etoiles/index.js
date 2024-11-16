2ec966f8ea1bda2769e6498605e4e6942f3b7890
Letocha Torgue Philippe <torguephilippe@gmail.com>
2024-11-16 11:21:29 +0100

    Update index.js

--- WebGL/etoiles/index.js
+++ WebGL/etoiles/index.js
@@ -331,6 +331,7 @@ gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,new Uint8Array([255, 0, 0, 255])); // red
 
 let renderVS=`
   attribute vec2 dataLocation;
