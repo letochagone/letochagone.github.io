a09ba366a1ddef6debc05b83e7fb96f3c33f3e53
Letocha Torgue Philippe <torguephilippe@gmail.com>
2024-11-18 10:40:27 +0100

    Update index.js

--- WebGL/etoiles/index.js
+++ WebGL/etoiles/index.js
@@ -41,7 +41,7 @@ function touch(event) {
     const x = (event.clientX / width) * 2 - 1;
     const y = (event.clientY / height) * -2 + 1;
     //remettre
-    if (!debug) emitParticles(x, y);
+    if (!debug) _emitParticles(x, y);
    // console.log(millis);
 
 }
